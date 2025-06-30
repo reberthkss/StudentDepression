9aa52e9f1973bf2a80c735a49d106b166ca9e1a4
Reberth <reberthkss@outlook.com>
2025-06-30 18:38:07 -0300

    feat: gender and age questions

--- student-depression/src/app/repository/question_data_source_response.tsx
+++ student-depression/src/app/repository/question_data_source_response.tsx
@@ -1,10 +1,24 @@
 const questions: QuestionInterface[] = [
     new Question(
         {
-            id: "123",
+            id: "1",
             type: QuestionType.option,
             title: "Qual seu genero?",
             description: "Descreva seu genero",
+            options: [
+                new ResponseOption("Masculino"),
+                new ResponseOption("Feminino"),
+                new ResponseOption("LGBTQIAPN+")
+            ],
+            response: undefined
+        }
+    ),
+    new Question(
+        {
+            id: "2",
+            type: QuestionType.text,
+            title: "Qual sua idade?",
+            description: "Sua idade influencia ....",
             options: [],
             response: undefined
         }
