1d1b5c997c8cd3ff81d04df419536eac2922b34e
Reberth <reberthkss@outlook.com>
2025-07-01 15:58:01 -0300

    improvement: repository improvements

--- student-depression/src/app/repository/question_respository.tsx
+++ student-depression/src/app/repository/question_respository.tsx
@@ -1,16 +1,16 @@
 import { questions } from "./question_data_source_response";
-import {QuestionInterface} from '../model/question';
+import { QuestionInterface } from '../model/question';
 export interface QuestionRepositoryInterface {
     fetch(): Promise<QuestionInterface[]>
     updateResponse(questionId: string, response: string): Promise<void>;
 }
 
 export class QuestionRepository implements QuestionRepositoryInterface {
-    private constructor() {}
+    private constructor() { }
     static instance = new QuestionRepository();
 
     async fetch(): Promise<QuestionInterface[]> {
-        return Promise.resolve([...questions]);
+        return [...questions];
     }
     async updateResponse(questionId: string, response: string): Promise<void> {
         console.log(`Response updated: Q${questionId} = ${response}`);
