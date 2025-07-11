500156745f552d6cbf7bc4a8c4f77ed700192866
Reberth <reberthkss@outlook.com>
2025-07-10 23:56:19 -0300

    feat: survey processing loading

---
+++ student-depression/public/animated/loading.tsx
@@ -0,0 +1,14 @@
+import { ReactElement } from "react";
+import "./loading.css";
+
+export function Loading(): ReactElement {
+    return (
+        <div className="loading">
+  <span></span>
+  <span></span>
+  <span></span>
+  <span></span>
+  <span></span>
+</div>
+    )
+}
